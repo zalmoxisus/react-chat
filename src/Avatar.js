99c7edd411655cd113a538bb466ffc2a40b519eb
Zalmoxisus <zalmoxisus@gmail.com>
2015-10-28 17:33:44 +0200

    Refactor Avatar component: Use arrow functions instead of classes

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,23 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import styles from './Chat.css';
 
-export default class Avatar extends Component {
-  static defaultProps = {
-    src: '',
-    name: ''
-  };
-
-  static propTypes = {
-    src: PropTypes.string,
-    name: PropTypes.string
-  };
-
-  render() {
-    return (
-      <div className={styles.avatar}>
-        { this.props.src ? <img  className={styles.img} src={this.props.src} /> : <div className={styles.txt}>{this.props.name[0]}</div> }
-      </div>
-    );
-  }
-
-}
+export default ({ src, name }) => (
+  <div className={styles.avatar}>
+    { src ? <img className={styles.img} src={src}/> : <div className={styles.txt}>{name[0]}</div> }
+  </div>
+);
