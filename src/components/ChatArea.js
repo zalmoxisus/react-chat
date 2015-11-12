e413cacebb87aacc4349185559c10ad550b3456f
nndio <anastasia.diordiev@gmail.com>
2015-11-12 22:43:01 +0200

    Autoscrolling of chatarea

--- src/components/ChatArea.js
+++ src/components/ChatArea.js
@@ -8,36 +8,60 @@ function getMine(owner) {
   return (mine === owner);
 }
 
-export default ({ messages }) => (
-  <div>
-    {
-      messages.map( message => {
-        return (<div key={message.id} className={styles.msgBox}>
-          {
-            !getMine(message.sender) &&
-            <Avatar style={styles.avatar}
-                    src={message.avatar}
-                    name={message.name}
-              />
-          }
-          <div className={getMine(message.sender) ? styles.arrowRight : styles.arrowLeft}></div>
-          <div key={message.msg} className={getMine(message.sender) ? styles.my_msg : styles.u_msg}>
-            <div className={styles.content_msg}> {message.msg} </div>
-            <div className={styles.footer_msg}>
-              {
-                !getMine(message.sender) &&
-                <div>
-                  <div style={{float: 'left'}}> {message.name} </div>
-                  <div className="icon-access-time"></div>
-                </div>
-              }
-              <div style={{float: 'left'}}> {getTimeStamp(message.time)} </div>
-            </div>
-          </div>
-        </div>
-        );
-      })
-    }
+export default class ChatArea extends Component {
+  static propTypes = {
+    messages: PropTypes.array
+  };
 
-  </div>
-);
+  componentDidMount() {
+    setTimeout(this.updateScrollTop, 500);
+  }
+
+  componentDidUpdate = () => {
+    this.updateScrollTop();
+  };
+
+  updateScrollTop = () => {
+    let node = document.getElementById('container');
+    if (!node) {
+      return;
+    }
+    node.scrollTop = node.scrollHeight;
+  };
+
+  render() {
+    return (
+      <div id="container" className={styles.container}>
+        {
+          this.props.messages.map( message => {
+            return (<div key={message.id} className={styles.msgBox}>
+                {
+                  !getMine(message.sender) &&
+                  <Avatar style={styles.avatar}
+                          src={message.avatar}
+                          name={message.name}
+                    />
+                }
+                <div className={getMine(message.sender) ? styles.arrowRight : styles.arrowLeft}></div>
+                <div key={message.msg} className={getMine(message.sender) ? styles.my_msg : styles.u_msg}>
+                  <div className={styles.content_msg}> {message.msg} </div>
+                  <div className={styles.footer_msg}>
+                    {
+                      !getMine(message.sender) &&
+                      <div>
+                        <div style={{float: 'left'}}> {message.name} </div>
+                        <div className="icon-access-time"></div>
+                      </div>
+                    }
+                    <div style={{float: 'left'}}> {getTimeStamp(message.time)} </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        }
+
+      </div>
+    );
+  }
+}
