942656196091c63eba088e23f6c66f51acfc561b
nndio <anastasia.diordiev@gmail.com>
2015-12-01 18:25:08 +0200

    Add smiley button

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -2,14 +2,16 @@ import React, { Component, PropTypes } from 'react';
 import styles from '../Chat.css';
 import TextareaAutosize from 'react-textarea-autosize';
 import UserMenu from './UserMenu';
+import {emojify} from 'react-emojione';
 
 export default class ChatInput extends Component {
   static propTypes = {
     onMessage: PropTypes.func
   };
   componentDidMount = () => {
-    let node = document.getElementsByTagName('ul')[0];
-    node.style.visibility = 'hidden';
+    let node = document.getElementsByTagName('ul');
+    node[0].style.visibility = 'hidden';
+    node[1].style.visibility = 'hidden';
   };
   hideMenu = (e) => {
     const node = document.getElementsByTagName('ul')[0];
@@ -34,6 +36,18 @@ export default class ChatInput extends Component {
       node.style.visibility = 'hidden';
     }
   };
+  hideEmoticons = (e) => {
+    const node = document.getElementsByTagName('ul')[1];
+    const this_ = e.currentTarget.getElementsByTagName('span')[1];
+    if (node.style.visibility === 'hidden') {
+      this_.style.transform = 'rotate(180deg)';
+      node.style.visibility = 'visible';
+    } else {
+      this_.style.transform = 'rotate(0deg)';
+      node.style.visibility = 'hidden';
+    }
+  };
+
   render() {
     const onMessage = this.props.onMessage;
     return (<div className={styles.chatInpContainer}>
@@ -53,6 +67,11 @@ export default class ChatInput extends Component {
         });
       }
       }/>
+        <div className={styles.emoticons} onClick={this.hideEmoticons}>
+          {emojify(' :) ')}
+          <ul>
+          </ul>
+        </div>
       </div>
     );
   }
