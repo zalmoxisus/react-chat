662343455e2aa1b83d64d01375b54bae19b6fa3b
nndio <anastasia.diordiev@gmail.com>
2016-01-11 22:21:45 +0200

    If message’s time is not from today show also date

--- src/utils/getTimeStamp.js
+++ src/utils/getTimeStamp.js
@@ -1,6 +1,16 @@
 export default function getTimeStamp(msgTime) {
-  let date = new Date(msgTime * 1000);
-  let hours = date.getHours();
-  let minutes = '0' + date.getMinutes();
-  return (hours + ':' + minutes.substr(-2));
+  const date = new Date(msgTime * 1000);
+  const day = date.getDate();
+  const minutes = '0' + date.getMinutes();
+
+  const todayTimeStamp = Date.now() / 1000 | 0;
+  const today = new Date(todayTimeStamp * 1000);
+
+  let time;
+  if (day === today.getDate()) {
+    time = (date.getHours() + ':' + minutes.substr(-2));
+  } else {
+    time = day + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() + ' ' + (date.getHours() + ':' + minutes.substr(-2));
+  }
+  return time;
 }
