b3af1ff168c87922896c899633df7e0fa23290c9
nndio <anastasia.diordiev@gmail.com>
2016-02-20 17:24:30 +0200

    Implement contact list example

---
+++ examples/contactlist/index.js
@@ -0,0 +1,5 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import ContactList from 'react-chat/ContactList';
+
+ReactDOM.render(<ContactList />, document.getElementById('root'));
---
+++ src/ContactList.js
@@ -0,0 +1,9 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class ContactList extends Component {
+  render() {
+    return (
+      <div>hi</div>
+    );
+  }
+}
