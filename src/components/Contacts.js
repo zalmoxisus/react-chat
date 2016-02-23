46c7c087a96fdee1388df82d35064f0061a2c6da
nndio <anastasia.diordiev@gmail.com>
2016-02-23 12:27:02 +0200

    Implement toggle left options for contact list

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import styles from '../ContactList.css';
 import MdKeyboardArrowDown from 'react-icons/lib/md/keyboard-arrow-down';
 import MdInfo from 'react-icons/lib/md/info';
+import MdMessage from 'react-icons/lib/md/message';
+import MdVideocam from 'react-icons/lib/md/videocam';
 
 export default class ContactList extends Component {
   static propTypes = {
@@ -16,11 +18,15 @@ export default class ContactList extends Component {
       this.avatar.children[0].style.width = '25px';
       this.avatar.children[0].style.height = '25px';
       this.avatar.children[0].style.fontSize = '12px';
+
+      this.optionsLeft.style.display = 'none';
     } else {
       node.style.transform = 'rotate(180deg)';
-      this.avatar.children[0].style.width = '50px';
-      this.avatar.children[0].style.height = '50px';
+      this.avatar.children[0].style.width = '48px';
+      this.avatar.children[0].style.height = '48px';
       this.avatar.children[0].style.fontSize = '23px';
+
+      this.optionsLeft.style.display = 'flex';
     }
   };
 
@@ -32,7 +38,14 @@ export default class ContactList extends Component {
             <img className={styles.img} src={this.props.src}/> :
             <span className={styles.txt}>{this.props.name[0]}</span>
         }
-        <span>{this.props.name}</span>
+        <span>
+          <span>{this.props.name}</span>
+          <div ref={(ref) => this.optionsLeft = ref} className={styles.optionsLeft}>
+            <MdInfo/>
+            <MdMessage/>
+            <MdVideocam/>
+          </div>
+        </span>
         <div className={styles.arrow} onClick={this.toggleInfo}><MdKeyboardArrowDown/></div>
       </li>
     );
