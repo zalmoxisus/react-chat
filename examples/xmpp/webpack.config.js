2d20227d7993da3259543395ab0260b8a39b9ef2
Zalmoxisus <zalmoxisus@gmail.com>
2016-03-11 14:21:07 +0200

    Clean up

--- examples/xmpp/webpack.config.js
+++ examples/xmpp/webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'eval',
   entry: [
     'webpack-hot-middleware/client',
     './index'
