2df277b54de287dcc37b0f3c2d42e5907af2c0d9
nndio <anastasia.diordiev@gmail.com>
2016-03-30 16:05:26 +0300

    Fix regex for link messages

--- src/components/messages/VideoContainer.js
+++ src/components/messages/VideoContainer.js
@@ -8,23 +8,24 @@ export default class VideoContainer extends Component {
     this.state = {
       emojiSrc: ''
     };
+    this.exp = /<img[^>]+>(<\/img>)?|<iframe.+?<\/iframe>|<a[^>]*>([^<]+)(<\/a>)?/ig;
   }
 
   componentWillMount() {
     this.setState({
-      emojiSrc: this.props.src.replace(/<img[^>]+>(<\/img>)?|<iframe.+?<\/iframe>/ig, '')
+      emojiSrc: this.props.src.replace(this.exp, '')
     });
   }
   componentDidMount() {
-    this.contentMsg.innerHTML = this.props.src.match(/<img[^>]+>(<\/img>)?|<iframe.+?<\/iframe>/ig);
+    this.contentMsg.innerHTML = this.props.src.match(this.exp);
   }
   componentWillReceiveProps() {
     this.setState({
-      emojiSrc: this.props.src.replace(/<img[^>]+>(<\/img>)?|<iframe.+?<\/iframe>/ig, '')
+      emojiSrc: this.props.src.replace(this.exp, '')
     });
   }
   componentDidUpdate() {
-    this.contentMsg.innerHTML = this.props.src.match(/<img[^>]+>(<\/img>)?|<iframe.+?<\/iframe>/ig);
+    this.contentMsg.innerHTML = this.props.src.match(this.exp);
     return true;
   }
   mapRef = (node) => {
