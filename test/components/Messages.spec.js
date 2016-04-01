66cdec6e48a9d5b070ebba1b1a683bc0e17041ec
nndio <anastasia.diordiev@gmail.com>
2016-04-01 13:35:14 +0300

    Render message test

--- test/components/Messages.spec.js
+++ test/components/Messages.spec.js
@@ -14,48 +14,17 @@ const props = {
     time: 1444428192,
     sender: 1
   },
-  messages: [
-    {
-      id: 0,
-      name: 'John',
-      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/oagra/128.jpg',
-      msg: 'Hello, Marry!',
-      time: 1444428192,
-      sender: 1
-    },
-    {
-      id: 1,
-      name: 'John',
-      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/oagra/128.jpg',
-      msg: 'Hello, Marry!',
-      time: 1444428192,
-      sender: 1
-    }
-  ],
   me: { id: 2 },
   isMine: id => props.me.id === id,
-  onTranslate: (txt, to, cb) => {
-    cb(txt);
-  },
-  translateLanguages: [
-    { c: 'sq', l: 'Albanian' },
-    { c: 'ar', l: 'Arabic' }
-  ],
-  onDelete: (message, success) => {
-    props.messages.forEach((item, index, object) => {
-      if (item.id === message) {
-        object.splice(index, 1);
-      }
-    });
-    success();
-  }
+  withPhoto: true
 };
 
 describe('Message', () => {
-  it('should delete message', () => {
+  it('should render message', () => {
     const wrapper = mount(<Message {...props} />);
-    expect(props.messages.length).toBe(2);
-    wrapper.find('.' + styles.secondCell).find('div').at(2).simulate('click');
-    expect(props.messages.length).toBe(1);
+    expect(wrapper.find('div').at(0).prop('className')).toBe(styles.msgBox);
+    const parentDiv = wrapper.find('.' + styles.msgBox).children();
+    expect(parentDiv.length).toBe(3);
+    expect(wrapper.find('.' + styles.msgBox).childAt(0).prop('className')).toBe(styles.avatar);
   });
 });
