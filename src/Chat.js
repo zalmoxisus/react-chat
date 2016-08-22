1b8f52b96bbce5a837a175e8e56e674c7aa06046
Anastasia_d <anastasia.diordiev@gmail.com>
2016-08-22 15:14:11 +0300

    Fix Chat component

--- src/Chat.js
+++ src/Chat.js
@@ -1,27 +1,15 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import ChatArea from './components/ChatArea';
 import ChatInput from './components/ChatInput';
 import styles from './chat.scss';
 
 export default class Chat extends Component {
-  replay = (e) => {
-    const name = e.currentTarget.textContent;
-    const node = this.input.usermsg;
-    node.value = name + ', ' + node.value;
-    node.focus();
-  };
-
   render() {
     return (
       <div className={styles.base}>
-        <ChatArea
-          replay={this.replay}
-        />
-        <ChatInput
-          ref={node => {this.input = node;}}
-        />
+        <ChatArea />
+        <ChatInput />
       </div>
     );
   }
 }
-
